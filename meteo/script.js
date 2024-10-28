155dd6c25890cf647d060f9fd6e06cbacac30d15
loris.martini <loris.martini@buonarroti.tn.it>
2024-10-28 11:36:05 +0100

    Package meteo

---
+++ meteo/script.js
@@ -0,0 +1,10 @@
+//const API_KEY = 'bd832622acc99b03e95f5648052a97cf';
+//const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=${units}&lang=${lang}`;
+
+// Recupero gli elementi di interesse dalla pagina
+const htmlElement = document.documentElement;
+const suggestion = document.querySelector('.suggestion');
+const weatherIcon = document.querySelector('.weather-icon');
+const weatherLocation = document.querySelector('.weather-location');
+const weatherTemperature = document.querySelector('.weather-temperature');
+
